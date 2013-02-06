129d6350b327012e91048c53a35fb22feb230975
lukaramishvili <1906262+lukaramishvili@users.noreply.github.com>
2013-02-06 11:12:05 +0400

    1.19 finished

--- 1_19/src/1_19.c
+++ 1_19/src/1_19.c
@@ -6,22 +6,27 @@
 /* maximum input line length */
 
 void our_copy(char to[], char from[]);
-char * get_full_line();
+char* get_full_line();
+char* reverse(char *s);
 
 /* print the longest input line */
 int main()
 {
   int len;       /* current line length */
   char* line = (char *) malloc(STARTSIZE);    /* current input line */
-  int last_loc, i;
 
   while ((len = strlen(line = get_full_line())) > 0) {
-	 for(last_loc = len-1; last_loc >= 0
-	 	 	 && (line[last_loc] == '\n' || line[last_loc] == ' ' || line[last_loc] == '\t');
-	 	 	 last_loc--);
-     for(i = 0; i <= last_loc; i++) printf("%c", line[i]);
+	 printf("%s", reverse(line));
   }
   return 0;
+}
+char* reverse(char *s){
+	int len, curs;
+	len = strlen(s);
+	curs = len;
+	char* v = malloc(len * sizeof(char));//although sizeof(char) is always 1
+	while(v[len-curs]=s[--curs]);
+	return v;
 }
 char* get_full_line(){
 	int next_size = STARTSIZE*sizeof(char);
