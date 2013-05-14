e4a7e33abbe2f218f5f2baf2cf854f87d0feda77
lukaramishvili <lukaramishvili@localhost>
2013-05-14 11:34:50 +0400

    also works for single-line comments

--- 1_23/src/1_23.c
+++ 1_23/src/1_23.c
@@ -11,15 +11,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define IN 1
 #define OUT 0
+#define IN 1
+#define IN_SINGLELINE 2
 
 int main(void) {
 	int prev=0,c, state = OUT;
 	while((c=getchar())!=EOF){
 		if(state == OUT){
-			if(c == '/'){ prev = c; continue; }
+			if(prev == '/' && c == '/'){ state = IN_SINGLELINE; }
 			else if(prev == '/' && c == '*'){ state = IN; }
+			else if(c == '/'){ prev = c; continue; }
 			else if(prev == '/'){ printf("%c%c",prev,c); }
 			else { printf("%c",c); }
 		}else if(state == IN){
@@ -27,8 +29,10 @@ int main(void) {
 			else if(prev == '*' && c == '/'){
 				state = OUT; prev = 0; continue;/* a slash ending a comment cannot start another one */
 			}
+		}else if(state == IN_SINGLELINE){
+			if(c == '\n'){ state = OUT; printf("%c",c); }
 		} else {/* no state other than IN/OUT */}
-		prev = c;
+		prev = c;//in soviet russia, this comment reads you!
 	}
 	return EXIT_SUCCESS;
 }
